62e5a23d42a7a156c0044ba5ab14363024c161d0
Sandro Vendeth <sandrohenrique92@outlook.com>
2023-06-01 13:55:17 -0300

    second commit

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import {
+    Container, Content
+} from './styles';
+
+export function Home() {
+ return (
+    <Container>
+        <Content>
+            OOOOOOOOOOOLÁ
+        </Content>
+    </Container>
+ );
+}
---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+    width: 100%;
+    height: 100%;
+    background-color: #FFF;
+    justify-content: center;
+`;
+export const Content = styled.Text`
+    color: red;
+
+    font-size: 30px;
+    text-align: center;
+`;
